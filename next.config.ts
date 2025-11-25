3fa99c45b614101bf38a13cf0899648d595a9f8d
bang26112001 <bang26112001@gmail.com>
2025-11-25 09:59:15 +0000

    feat: base code add scss

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: `
+    @import "@Styles/_tool.scss";
+    @import "@Styles/_variable.scss";
+      `,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
