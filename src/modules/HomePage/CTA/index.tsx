a916a2546a8b72ad82134e4aa6015a9cd381d01c
bang26112001 <bang26112001@gmail.com>
2026-01-14 09:21:10 +0000

    feat: how it work + cta section

---
+++ src/modules/HomePage/CTA/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import UIButton from '@Components/Button';
+import Image from 'next/image';
+
+import s from './styles.module.scss';
+
+const CTA = () : React.ReactElement => {
+  return (
+    <section className={s.cta}>
+      <div className={`${s.cta_main} container`}>
+        <div className={`${s.cta_bottom} grid grid-cols-12`}>
+          <div className={`${s.cta_content} txt-light`}>
+            <div className={`${s.cta_content_txt} txt-light`}>
+              Ready to Ride the Future?
+            </div>
+            <UIButton color='secondary'>Get Your Drone</UIButton>
+          </div>
+        </div>
+      </div>
+      <div className={s.cta_bg}>
+        <Image
+          className={s.cta_bg_image}
+          src="/images/cta-image.png"
+          alt=""
+          height="1700"
+          width="2880"
+        ></Image>
+      </div>
+    </section>
+  );
+};
+
+export default CTA;
---
+++ src/modules/HomePage/HowItWork/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Container from '@Components/Container';
+import Image from 'next/image';
+
+import s from './styles.module.scss';
+
+const HowItWork = (): React.ReactElement => {
+  return (
+    <section className={s.howitwork}>
+      <Container>
+        <div className={s.howitwork_header}>
+          <span className={`${s.howitwork_subtitle} txt-med`}>How It Works</span>
+          <h5 className={`${s.howitwork_title} txt-light`}>Fly in 3 Simple Steps</h5>
+        </div>
+        <div className={s.steps}>
+          <div className={s.step_item}>
+            <div className={s.step_item_content}>
+              <div className={s.step_item_number}>1</div>
+              <div className={s.step_item_title}>Choose Your Drone</div>
+              <div className={s.step_item_text}>Find the model that suits your needs</div>
+            </div>
+            <Image
+              className={s.step_item_image}
+              src="/images/step1.png"
+              alt=""
+              height="570"
+              width="427"
+            />
+            <div className={s.step_item_overlay}></div>
+          </div>
+          <div className={s.step_item}>
+            <div className={s.step_item_content}>
+              <div className={s.step_item_number}>2</div>
+              <div className={s.step_item_title}>Unbox & Setup</div>
+              <div className={s.step_item_text}>Easy setup with our quick-start guide</div>
+            </div>
+            <Image
+              className={s.step_item_image}
+              src="/images/step2.png"
+              alt=""
+              height="570"
+              width="427"
+            />
+            <div className={s.step_item_overlay}></div>
+          </div>
+          <div className={s.step_item}>
+            <div className={s.step_item_content}>
+              <div className={s.step_item_number}>3</div>
+              <div className={s.step_item_title}>Take Off & Capture</div>
+              <div className={s.step_item_text}>Enjoy high-quality aerial views</div>
+            </div>
+            <Image
+              className={s.step_item_image}
+              src="/images/step3.png"
+              alt=""
+              height="570"
+              width="427"
+            />
+            <div className={s.step_item_overlay}></div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default HowItWork;
