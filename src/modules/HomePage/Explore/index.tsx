ce45e4ebdc30ebbfbe65612d5cb7a4d122492bb8
396_Nguyễn Đức Bằng <0306191396@caothang.edu.vn>
2026-05-21 09:31:23 +0000

    feat: add anim home explore

--- src/modules/HomePage/Explore/index.tsx
+++ src/modules/HomePage/Explore/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import UIButton from '@Components/Button';
 import Container from '@Components/Container';
+import Fade from '@Components/FadeAnim';
+import TypoAnim from '@Components/TypoAnim';
 import Image from 'next/image';
 
 import Slider from './Slider';
@@ -13,14 +15,24 @@ const Explore = (): React.ReactElement => {
       <section className={s.home_explore}>
         <Container>
           <div className={s.home_explore__header}>
-            <span className={`${s.home_explore__subtitle} txt-med`}>Explore Our Models</span>
-            <h5 className={`${s.home_explore__title} txt-light`}>Choose Your Perfect Drone</h5>
-            <p className={s.home_explore__description}>Find the best drone for your needs.</p>
+            <TypoAnim delayTrigger={0.3} stagger={0.2}>
+              <span className={`${s.home_explore__subtitle} txt-med`}>Explore Our Models</span>
+            </TypoAnim>
+            <TypoAnim delayTrigger={0.3} stagger={0.2}>
+              <h5 className={`${s.home_explore__title} txt-light`}>Choose Your Perfect Drone</h5>
+            </TypoAnim>
+            <TypoAnim delayTrigger={0.3} stagger={0.2}>
+              <p className={s.home_explore__description}>Find the best drone for your needs.</p>
+            </TypoAnim>
           </div>
 
           <div className={s.slider_title_wrap}>
-            <h5 className={`${s.slider__title}`}>Drone Model</h5>
-            <UIButton>View All Models</UIButton>
+            <Fade direction="bottom" from="10px" delayTrigger={1}>
+              <h5 className={`${s.slider__title}`}>Drone Model</h5>
+            </Fade>
+            <Fade direction="bottom" from="10px" delayTrigger={1.1}>
+              <UIButton>View All Models</UIButton>
+            </Fade>
           </div>
         </Container>
 
@@ -28,9 +40,6 @@ const Explore = (): React.ReactElement => {
       </section>
 
       <div className={s.ticker_bg_wrap}>
-        <div className={`${s.ticker_title_wrap} container`}>
-          <h5 className={`${s.ticker_title}`}>FlyBeyond</h5>
-        </div>
         <div className={s.ticker_bg}>
           <Image
             className={s.ticker_bg__image}
