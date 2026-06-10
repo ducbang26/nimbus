ad9a3b963db79fcc8682b74075a3533dc0b998cf
_bangcumtay <58516805+ducbang26@users.noreply.github.com>
2026-06-10 08:51:46 +0000

    feat: fix build route api

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,18 +1,8 @@
 import { NextResponse } from 'next/server';
-import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16' as any,
-});
+export const runtime = 'nodejs';
 
-export async function POST(req: Request): Promise<
-  | NextResponse<{
-      url: string | null;
-    }>
-  | NextResponse<{
-      error: any;
-    }>
-> {
+export async function POST(req: Request) {
   try {
     const { cartItems } = await req.json();
 
@@ -20,12 +10,22 @@ export async function POST(req: Request): Promise<
       return NextResponse.json({ error: 'Cart is empty' }, { status: 400 });
     }
 
+    const Stripe = (await import('stripe')).default;
+
+    if (!process.env.STRIPE_SECRET_KEY) {
+      throw new Error('Missing STRIPE_SECRET_KEY');
+    }
+
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2023-10-16' as any,
+    });
+
     const line_items = cartItems.map((item: any) => ({
       price_data: {
         currency: 'usd',
         product_data: {
           name: item.name || 'Product Name',
-          images: item.images?.[0]?.url ? [item.images?.[0]?.url] : [],
+          images: item.images?.[0]?.url ? [item.images[0].url] : [],
         },
         unit_amount: Math.round(item.price * 100),
       },
@@ -34,7 +34,7 @@ export async function POST(req: Request): Promise<
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
-      line_items: line_items,
+      line_items,
       mode: 'payment',
       success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cart`,
